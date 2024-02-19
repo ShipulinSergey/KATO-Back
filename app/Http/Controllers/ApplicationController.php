7123d9040471834e00cd8f0e58348c418b8398e4
Shipulin Sergey <70929955+Diego475@users.noreply.github.com>
2024-02-19 10:09:43 +0600

    hot fix

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -12,6 +12,7 @@ class ApplicationController extends Controller
     public function store(ApplicationRequest $request)
     {
         $data = $request->validated();
+        $data['form'] = 1;
         $application = Application::create($data);
         if (!$application) {
             return response()->json(['success' => false, 'message' => 'Failed to save application']);
